839b00893f928420d49d6757815f14f054a42e10
BradieF <bradiefuentes2002@gmail.com>
2025-02-10 13:37:11 -0500

    added hover function to buttons and links and fixed grid system on Row 4, added javascript file

---
+++ javascript.js
@@ -0,0 +1,12 @@
+const track = document.querySelector('.reviewTrack');
+const slides = Array.from(track.children)
+
+const nextButton = document.querySelector('.rightButton')
+const previousButton = document.querySelector('.leftButton')
+
+const dotNavs = document.querySelector('.slideNav')
+const dots = Array.from(dotNavs.children)
+
+//click left to move slide left
+//click right to move slide right
+//click indicator to navigate to slide
